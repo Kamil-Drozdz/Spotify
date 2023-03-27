a7f9e5ce74b2e7b4c95eb070d9569d1c3fd85a12
Kamil Dróżdż <kamil12x@wp.pl>
2023-03-28 00:21:14 +0200

    hotfix path

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 import eslint from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-plugin-tsconfig-paths';
 
 const resolve = {
-  alias: {
-    "@": fileURLToPath(new URL("./src", import.meta.url)),
-    "@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
-  },
+	alias: {
+		'@': fileURLToPath(new URL('./src', import.meta.url)),
+		'@modules': fileURLToPath(new URL('./src/modules', import.meta.url)),
+	},
 };
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    devSourcemap: true,
-  },
-  plugins: [react(), eslint()],
-  resolve,
+	css: {
+		devSourcemap: true,
+	},
+	plugins: [react(), eslint(), tsconfigPaths()],
+	resolve,
 });
